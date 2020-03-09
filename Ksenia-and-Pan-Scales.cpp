626bcd170791f887297932c47e2ef563e5814b0d
NProof <34260623+NProof@users.noreply.github.com>
2020-03-09 18:33:55 +0800

    be sure LETTER is difference !

--- Ksenia-and-Pan-Scales.cpp
+++ Ksenia-and-Pan-Scales.cpp
@@ -1,11 +1,17 @@
 // [https://codeforces.com/contest/382/problem/A]
 #include <iostream>
 
+#define MAX_SIZE 37
+
 int main(int argc, char **argv) {
+  char str1[MAX_SIZE], str2[MAX_SIZE];
+  int i; // index of str
   int c; // character storage
   long lLeft = 0, lRight = 0, lSecond = 0;
   bool passed = false;
+  i = 0;
   while((c = std::cin.get()) != '\n' && c != EOF) {
+    str1[i++] = c;
     if(c == '|')
       passed = true;
     else if(passed) {
@@ -15,17 +21,25 @@ int main(int argc, char **argv) {
       ++lLeft;
     }
   }
+  str1[i] = '\0';
+  i = 0;
   while((c = std::cin.get()) != '\n' && c != EOF) {
+    str2[i++] = c;
     ++lSecond;
   }
+  str2[i] = '\0';
   if(lLeft > lRight) {
     if(lLeft != lRight + lSecond)
       printf("Impossible\n");
+    else
+      printf("%s%s\n", str1, str2);
   }
   else {
     if(lRight != lLeft + lSecond) {
       printf("Impossible\n");
     }
+    else
+      printf("%s%s\n", str2, str1);
   }
   return 0;
 }
